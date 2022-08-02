7b878b790de1e9aeb8c531adb9c67450c4a97ade
Tim Fawcett <timfawcett21@gmail.com>
2022-08-02 21:05:41 +1000

    API Calls to Get Random Country + Country Data

--- script.js
+++ script.js
@@ -1,48 +1,27 @@
 'use strict';
-
-const allCountries = [];
-const getAllCountries = function () {
-  fetch('https://restcountries.com/v3.1/all').then(function(response) {
-  return response.json();
-}).then(function (data) {
-  data.forEach(country => console.log(country.name.common));
-  data.forEach(c => allCountries.push(c));
-  // data.forEach(country => allCountries.push(country));
-  // console.log(data.slice(2));
-  // allCountries = Object.keys(data);
-});
-}
-
-
-
-getAllCountries();
-console.log(allCountries);
-const randomCountry = allCountries.map(country => country.name);
-console.log('testingtestingtesting');
-console.log(randomCountry);
-// console.log(allCountries);
-// // // console.log(Object.keys(getAllCountries()));
-// // // console.log(allCountries);
-
-// // // const randomCountry = allCountries.map(country => country.name.common);
-// // // console.log(randomCountry);
-// let allCountryData = [];
-// function getAllCountries() {
-//   fetch('https://restcountries.com/v3.1/all').then(function(response) {
-//     return response.json();
-//   }).then(function (data) {
-//     data.forEach(country => allCountryData.push(country));
-//   })
-// };
-
-// // const countries = getAllCountries();
-// getAllCountries();
-// // console.log(allCountryData);
-// // const countryNames = allCountryData.map(country => country.name);
-// // console.log(countryNames);
-// const randomCountry = function () {
-//   const c = allCountryData.forEach(country => country.name.common);
-//   console.log(c);
-// }
-
-// randomCountry();
+const countriesCard = document.querySelector('country-card');
+const getRandomCountryWithData = function () {
+  fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(function (data) {
+      const allCountries = [];
+      data.forEach(country => allCountries.push(country.name.common));
+      const country = allCountries[`${Math.floor(Math.random() * (250 - 0 + 1)) + 0}`];
+      // console.log(country);
+      // return allCountries[`${randomNum(0, 250)}`];
+      return fetch(`https://restcountries.com/v3.1/name/${country}`);
+    })
+    .then(response => response.json())
+    .then(function (data) {
+      const result = {
+      name: data[0].name.common,
+      size: data[0].area,
+      population: data[0].population,
+      continent: data[0].continents[0],
+      flag: data[0].flags.png
+      };
+      console.log(result);
+      // return result;
+    })
+};
+getRandomCountryWithData();
