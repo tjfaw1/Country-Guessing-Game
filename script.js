199e358646304dc5b4cd12798584d512a13870df
Tim Fawcett <timfawcett21@gmail.com>
2022-08-05 18:32:00 +1000

    scoring and reset button functionality

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 'use strict';
 const countriesCard = document.querySelector('.country-card');
 const introBox = document.querySelector('.intro');
-const getRandomCountryWithData = function () {
+const guess = document.querySelector('.guess-bar').value;
+const message = document.querySelector('.results').textContent;
+
+let randomCountry;
+const getRandomCountryAndDataFromApi = function () {
   fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(function (data) {
       const allCountries = [];
       data.forEach(country => allCountries.push(country.name.common));
       const country = allCountries[`${Math.floor(Math.random() * (250 - 0 + 1)) + 0}`];
-      // console.log(country);
+      console.log(country);
       // return allCountries[`${randomNum(0, 250)}`];
       return fetch(`https://restcountries.com/v3.1/name/${country}`);
     })
     .then(response => response.json())
     .then(function (data) {
-      console.log(data[0].name.common);
+      randomCountry = data[0].name.common;
       const html = `
         <div class="population" id="country-snippet">
           Population: ${(+data[0].population/1000000).toFixed(1) + ' million'}
@@ -30,17 +34,16 @@ const getRandomCountryWithData = function () {
         </div>
         <div class="flag" id="country-snippet">
           <div class="flag-container">
-            <img src="${data[0].flags.png}" alt="" style="height: 120px; width: 200px;">
+            <img src="${data[0].flags.png}" alt="" style="height: 120px; width: 200px; border: 1px solid black;">
           </div>
         </div>
       `;
       countriesCard.insertAdjacentHTML('beforeend', html);
-      // countriesCard.style.opacity = 1;
-
+      // document.querySelector('.results').style.opacity = 1;
     })
 };
 
-getRandomCountryWithData();
+getRandomCountryAndDataFromApi();
 
 // const play = function () {
 //   getRandomCountryWithData();
@@ -48,3 +51,51 @@ getRandomCountryWithData();
 // };
 
 // document.getElementById('play-btn').addEventListener('click', play);
+
+let score = 5;
+let highScore = 0;
+
+document.querySelector('.guess-bar').addEventListener('change', function () {
+  const guess = this.value;
+  if (guess == randomCountry) {
+    document.querySelector('.results').style.opacity = 1;
+    document.querySelector('.results').textContent = `Correct Guess! You got it in ${score} hits`;
+    document.querySelector('.results').style.backgroundColor = '#60b347';
+    document.querySelector('.results').style.color = 'white';
+    document.querySelector('.results').style.fontSize = 'larger';
+    document.querySelector('.results').style.fontWeight = 'bolder';
+
+    if(score > highScore){
+      highScore = score;
+      document.querySelector('#high-score').textContent = `High Score: ${highScore}`;
+    }
+
+  } else {
+    this.value = "";
+    if(score >= 1) {
+      document.querySelector('.results').style.opacity = 1;
+      document.querySelector('.results').textContent = 'Wrong Guess! Try Again';
+      score--;
+      document.querySelector('#current-score').textContent = `Current Score: ${score}`;
+    } else {
+      document.querySelector('.results').textContent = 'Game Over, You Lost!';
+    }
+    document.querySelector('.results').style.backgroundColor = 'red';
+    document.querySelector('.results').style.color = 'white';
+    document.querySelector('.results').style.fontSize = 'larger';
+    document.querySelector('.results').style.fontWeight = 'bolder';
+  }
+  console.log(guess);
+  console.log(`${randomCountry} is the random country`);
+});
+
+const reset = function () {
+  score = 5;
+  document.querySelector('#current-score').textContent = `Current Score: ${score}`;
+  document.querySelector('.results').style.opacity = 0;
+  document.getElementById('cc').innerHTML = ""
+  getRandomCountryAndDataFromApi();
+  // location.reload();
+};
+
+document.getElementById('reset-btn').addEventListener('click', reset);
